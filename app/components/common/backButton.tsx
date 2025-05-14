281b47f1874514e0d55820fd1af430d2c4b87f0f
CraftsManShip001 <seoj080829@gmail.com>
2025-05-15 05:02:59 +0900

    docs :: backButton 코드 리뷰 주석 추가

--- app/components/common/backButton.tsx
+++ app/components/common/backButton.tsx
@@ -7,9 +7,9 @@ export default function BackButton() {
   return (
     <TouchableOpacity
       onPress={() => navigation.goBack()}
-      style={{flexDirection:'row', alignItems:'center'}}
+      style={{flexDirection:'row', alignItems:'center'}} {/* 이와 같은 컴포넌트에서는 유지보수의 편의를 위해 stylesheet로 빼주는 것이 더 좋을 것 같습니다 */}
     >
-      <Text style={{fontSize:15, fontWeight:'bold'}}>←나가기</Text>
+      <Text style={{fontSize:15, fontWeight:'bold'}}>←나가기</Text> {/* 정적인 텍스트보다 label 같은 props를 통해 받는 것이 좋을 것 같습니다 또한 이 역시 공통 스타일로 분리시키는게 좋을 것 같습니다*/}
     </TouchableOpacity>
   );
-}
+} // 커스텀이 부가적으로 되지 않는 컴포넌트라서 확장성이 아쉬운 것 같습니다
